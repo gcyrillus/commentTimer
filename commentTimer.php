fa427fd9c2120baf6df7ec315d2d98dec8ca1f8f
gcyrillus <1832331+gcyrillus@users.noreply.github.com>
2023-04-07 09:58:24 +0200

    Update commentTimer.php

--- commentTimer.php
+++ commentTimer.php
@@ -4,13 +4,13 @@
     }
     
     class commentTimer extends plxPlugin {
-		
-		const HOOKS = array(
+        
+        const HOOKS = array(
         'plxMotorParseArticle',
         );
         const BEGIN_CODE = '<?php' . PHP_EOL;
         const END_CODE = PHP_EOL . '?>';
-		
+        
         public function __construct($default_lang) {
             # appel du constructeur de la classe plxPlugin (obligatoire)
             parent::__construct($default_lang);
@@ -23,29 +23,20 @@
             # droits pour accéder à la page config.php du plugin
             $this->setConfigProfil(PROFIL_ADMIN);
         }
-		
+        
         public function plxMotorParseArticle() {
             $validity= $this->getParam('com_OOD');
-            
-			
             echo self::BEGIN_CODE;
         ?>		
-		if($art['allow_com'] == 1) {
+        if($art['allow_com'] && $this->aConf['allow_com']) {
         $publicationDay = floor((strtotime(date('YmdHi')) - strtotime($art['date'])) / (60*60*24));
         $updateDay      = floor((strtotime(date('YmdHi')) - strtotime($art['date_update'])) / (60*60*24));
         $delay = '<?php echo $validity; ?>'; 
-        if (min($updateDay,$publicationDay) > $delay ) {$allow='0';}
-        else {$allow = '1';}
-        
-        $art['allow_com']= $allow;
+        if (min($updateDay,$publicationDay) > $delay ) { $art['allow_com']= 0; }
         }
-        
         
         <?php
             echo self::END_CODE;						
-        }		
-        
-
-        
+        }	     
     }
 ?>
